3ef66e4206c1d334b7fdaf2886c6daa0a30fced1
黒木　庸平 <>
2024-01-15 15:12:41 +0900

    [update]イラスト追加

--- index.php
+++ index.php
@@ -20,45 +20,58 @@
 <?php include( $_SERVER['DOCUMENT_ROOT'] . '/include/header.php'); ?>
 	
 <section id="gallery">
-	
-	<div class="container-five01">
-		<div class="item box01"><a href="/illustration/yoru.php"><img src="img/illustration/thum_yoru.png" alt="夜　a night owl"></a></div>
-		<div class="item box02"><a href="/illustration/merry_christmas.php"><img src="img/illustration/thum_merry_christmas.png" alt="merry_christmas"></a></div>
-		<div class="item box03"><a href="illustration/morning-coffee.php"><img src="img/illustration/thum_morning-coffee.png" alt="morning-coffee"></a></div>
-		<div class="item box04"><a href="/illustration/morning-jog.php"><img src="img/illustration/thum_morning-jog.png" alt="morning-jog"></a></div>
-		<div class="item box05"><a href="/illustration/bouquet.php"><img src="img/illustration/thum_bouquet.png" alt="bouquet"></a></div>
-	</div>
 	<div class="container-five02">
-		<div class="item box06"><a href="illustration/midsummer-diner.php"><img src="img/illustration/thum_midsummer-diner.png" alt="midsummer-diner"></a></div>
-		<div class="item box07"><a href="/illustration/summer-girl_02.php"><img src="img/illustration/thum_summergirl_02.png" alt="Summer girl #02"></a></div>
-		<div class="item box08"><a href="/illustration/ambush.php"><img src="img/illustration/thum_ambush.png" alt="ambush"></a></div>
-		<div class="item box09"><a href="/illustration/chinese-food.php"><img src="img/illustration/thum_chinese_food.png" alt="chinese_food"></a></div>
-		<div class="item box10"><a href="/illustration/halloween.php"><img src="img/illustration/thum_halloween.png" alt="Happy halloween!"></a></div>
+		<div class="item box06"><a href="illustration/alice.php"><img src="img/illustration/thumb/alice.png" alt="アリスとうさぎ"></a></div>
+		<div class="item box07"><a href="/illustration/catdays.php"><img src="img/illustration/thumb/catdays.png" alt="猫の日"></a></div>
+		<div class="item box08"><a href="/illustration/dragon.php"><img src="img/illustration/thumb/dragon.png" alt="A Girl and Her Dragon"></a></div>
+		<div class="item box09"><a href="/illustration/poolside.php"><img src="img/illustration/thumb/poolside.png" alt="プールサイド"></a></div>
+		<div class="item box10"><a href="/illustration/halloween02.php"><img src="img/illustration/thumb/halloween02.png" alt="Have a boo-tiful Halloween！"></a></div>
 	</div>
 	<div class="container-four">
-		<div class="item"><a href="/illustration/asahi-chukousei.php"><img src="img/illustration/thum_asahichuukousei.png" alt="高校入試　合格ごよみ（朝日中高生新聞）"></a></div>
-		<div class="item"><a href="/illustration/manmaru.php"><img src="img/illustration/thum_manmaru.png" alt="ウェブメディア（転職情報サイト）"></a></div>
-		<div class="item"><a href="/illustration/conceptwork.php"><img src="img/illustration/thum_conceptwork.png" alt="会社案内パンフレット"></a></div>
-		<div class="item"><a href="/illustration/hona.php"><img src="img/illustration/thum_hona.png" alt="業界職種勉強会チラシ"></a></div>
+		<div class="item"><a href="/illustration/ittatshumori.php"><img src="img/illustration/thumb/itta.png" alt="とっさの会話力と自信が身につく 行ったつもりで英会話（Gakken）"></a></div>
+		<div class="item"><a href="/illustration/mononoshinka.php"><img src="img/illustration/thumb/mono.png" alt="モノの進化まるわかり事典〈第2巻〉（Gakken）"></a></div>
+		<div class="item"><a href="/illustration/summer_time_in_tokyo01.php"><img src="img/illustration/thumb/summer_time_in_tokyo01.png" alt="summer_time_in_tokyo01"></a></div>
+		<div class="item"><a href="/illustration/summer_time_in_tokyo02.php"><img src="img/illustration/thumb/summer_time_in_tokyo02.png" alt="summer_time_in_tokyo02"></a></div>
 	</div>
+	<div class="container-five01">
+		<div class="item box01"><a href="/illustration/yoru.php"><img src="img/illustration/thumb/yoru.png" alt="夜　a night owl"></a></div>
+		<div class="item box02"><a href="/illustration/merry_christmas.php"><img src="img/illustration/thumb/merry_christmas.png" alt="merry_christmas"></a></div>
+		<div class="item box03"><a href="illustration/morning-coffee.php"><img src="img/illustration/thumb/morning-coffee.png" alt="morning-coffee"></a></div>
+		<div class="item box04"><a href="/illustration/morning-jog.php"><img src="img/illustration/thumb/morning-jog.png" alt="morning-jog"></a></div>
+		<div class="item box05"><a href="/illustration/bouquet.php"><img src="img/illustration/thumb/bouquet.png" alt="bouquet"></a></div>
+	</div>
+	<div class="container-four">
+		<div class="item"><a href="/illustration/asahi-chukousei.php"><img src="img/illustration/thumb/asahichuukousei.png" alt="高校入試　合格ごよみ（朝日中高生新聞）"></a></div>
+		<div class="item"><a href="/illustration/manmaru.php"><img src="img/illustration/thumb/manmaru.png" alt="ウェブメディア（転職情報サイト）"></a></div>
+		<div class="item"><a href="/illustration/conceptwork.php"><img src="img/illustration/thumb/conceptwork.png" alt="会社案内パンフレット"></a></div>
+		<div class="item"><a href="/illustration/hona.php"><img src="img/illustration/thumb/hona.png" alt="業界職種勉強会チラシ"></a></div>
+	</div>
+	<div class="container-five02">
+		<div class="item box06"><a href="illustration/midsummer-diner.php"><img src="img/illustration/thumb/midsummer-diner.png" alt="midsummer-diner"></a></div>
+		<div class="item box07"><a href="/illustration/summer-girl_02.php"><img src="img/illustration/thumb/summergirl_02.png" alt="Summer girl #02"></a></div>
+		<div class="item box08"><a href="/illustration/ambush.php"><img src="img/illustration/thumb/ambush.png" alt="ambush"></a></div>
+		<div class="item box09"><a href="/illustration/chinese-food.php"><img src="img/illustration/thumb/chinese_food.png" alt="chinese_food"></a></div>
+		<div class="item box10"><a href="/illustration/halloween.php"><img src="img/illustration/thumb/halloween.png" alt="Happy halloween!"></a></div>
+	</div>
+	
 	<div class="container-three">
-		<div class="item"><a href="/illustration/go-to-space.php"><img src="img/illustration/thum_space.png" alt="Let's go to space!"></a></div>
-		<div class="item"><a href="/illustration/happy-holidays.php"><img src="img/illustration/thum_happyholidays.png" alt="Happy holidays!"></a></div>
-		<div class="item"><a href="/illustration/spring.php"><img src="img/illustration/thum_spring.png" alt="Spring"></a></div>
+		<div class="item"><a href="/illustration/go-to-space.php"><img src="img/illustration/thumb/space.png" alt="Let's go to space!"></a></div>
+		<div class="item"><a href="/illustration/happy-holidays.php"><img src="img/illustration/thumb/happyholidays.png" alt="Happy holidays!"></a></div>
+		<div class="item"><a href="/illustration/spring.php"><img src="img/illustration/thumb/spring.png" alt="Spring"></a></div>
 		
 	</div>
 	<div class="container-three">
-		<div class="item"><a href="/illustration/monkey-girl.php"><img src="img/illustration/thum_monkeygirl.png" alt="Monkey girl"></a><</div>
-		<div class="item"><a href="/illustration/summer-girl_01.php"><img src="img/illustration/thum_summergirl_01.png" alt="Summer girl #01"></a></div>
-		<div class="item"><a href="/illustration/distracted-boy.php"><img src="img/illustration/thum_distracted_boy.png" alt="Distracted boy"></a></div>
+		<div class="item"><a href="/illustration/monkey-girl.php"><img src="img/illustration/thumb/monkeygirl.png" alt="Monkey girl"></a><</div>
+		<div class="item"><a href="/illustration/summer-girl_01.php"><img src="img/illustration/thumb/summergirl_01.png" alt="Summer girl #01"></a></div>
+		<div class="item"><a href="/illustration/distracted-boy.php"><img src="img/illustration/thumb/distracted_boy.png" alt="Distracted boy"></a></div>
 	</div>
 	
 	<!--<div class="container-five02">
-		<div class="item box06"><a href="/illustration/spring.php"><img src="img/illustration/thum_spring.png" alt=""></a></div>
-		<div class="item box07"><a href="/illustration/monkey-girl.php"><img src="img/illustration/thum_monkeygirl.png" alt=""></a></div>
-		<div class="item box08"><a href="/illustration/go-to-space.php"><img src="img/illustration/thum_space.png" alt=""></a></div>
-		<div class="item box09"><a href="/illustration/summer-girl_01.php"><img src="img/illustration/thum_summergirl_01.png" alt=""></a></div>
-		<div class="item box10"><a href="/illustration/distracted-boy.php"><img src="img/illustration/thum_distracted_boy.png" alt=""></a></div>
+		<div class="item box06"><a href="/illustration/spring.php"><img src="img/illustration/thumb/spring.png" alt=""></a></div>
+		<div class="item box07"><a href="/illustration/monkey-girl.php"><img src="img/illustration/thumb/monkeygirl.png" alt=""></a></div>
+		<div class="item box08"><a href="/illustration/go-to-space.php"><img src="img/illustration/thumb/space.png" alt=""></a></div>
+		<div class="item box09"><a href="/illustration/summer-girl_01.php"><img src="img/illustration/thumb/summergirl_01.png" alt=""></a></div>
+		<div class="item box10"><a href="/illustration/distracted-boy.php"><img src="img/illustration/thumb/distracted_boy.png" alt=""></a></div>
 	</div>-->
 	
 	<!--<div class="container-two">
