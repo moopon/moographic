45756f075e776a9778dd52497ba9904cbfb8f044
黒木　庸平 <>
2024-06-13 09:50:16 +0900

    [update]Googleアナリティクスタグを再度挿入

--- include/google_analytics-tag.php
+++ include/google_analytics-tag.php
@@ -1,11 +1,11 @@
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-4CNWZD9HB2"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-59CQB5EQ27"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-4CNWZD9HB2');
+  gtag('config', 'G-59CQB5EQ27');
 </script>
 
 <!-- Google Tag Manager -->
