756104b33e7e549b0039dac8cad7c61d635fce7f
黒木　庸平 <>
2024-06-23 21:31:37 +0900

    [update]住所変更

--- about/index.php
+++ about/index.php
@@ -36,7 +36,7 @@
 		<div class="about-box-txt">
 			<p class="content-txt">ムーグラフィックは、東京を中心に活動しているイラストレーターです。<br>アメリカンレトロスタイルや日本の漫画などに強い影響を受けた、ポップで明るいタッチのスタイルが特徴です。</p>
 			
-			<p class="content-txt">千葉県出身。現在は東京都世田谷区在住。<br>多摩美術大学美術学部グラフィックデザイン学科卒。複数のデザイン会社、ウェブ制作会社を経てフリーランスとして独立。<br><a href="https://iratsu.com/u1317/" target="_blank">イラストレーターズ通信</a>会員。</p>
+			<p class="content-txt">千葉県出身。現在は東京都墨田区在住。<br>多摩美術大学美術学部グラフィックデザイン学科卒。複数のデザイン会社、ウェブ制作会社を経てフリーランスとして独立。<br><a href="https://iratsu.com/u1317/" target="_blank">イラストレーターズ通信</a>会員。</p>
 
 			<p class="content-txt en">Moographic is a freelance illustrator living and working in Tokyo,Japan.<br>
 		I graduated from the Department of Graphic Design, Faculty of Fine Arts, Tama Art University.<br>My illustration is inspired by retro advertisements and comics.</p>
@@ -48,7 +48,7 @@
 				</tr>
 				<tr>
 					<th class="profile-table-heading">所在地：</th>
-					<td class="profile-table-txt">東京都世田谷区</td>
+					<td class="profile-table-txt">東京都墨田区</td>
 				</tr>
 				<tr>
 					<th class="profile-table-heading">事業内容：</th>
