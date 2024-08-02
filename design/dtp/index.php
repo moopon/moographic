d1a2b0b27c088f9deea3cf952a51b7393b57692d
黒木　庸平 <>
2024-08-02 11:11:08 +0900

    [update/add]DTP実績の追加

--- design/dtp/index.php
+++ design/dtp/index.php
@@ -34,7 +34,34 @@
 	<h2 class="content-title">DTP</h2>
 	
 	<div class="design-wrap">
-		<div class="design-pic"><img src="/img/design/dtp/Pamphlet-omote.png" alt=""></div>
+		<div class="design-pic"><img src="/img/design/dtp/croco_booklet.png" alt="会社案内資料 / CROCO株式会社"></div>
+		<div class="design-txt">
+			<h3 class="design-txt-heading">会社案内資料 / CROCO株式会社</h3>
+			<p class="design-txt-description">渋谷区のコンテンツマーケティングソリューションを開発・提供しているCROCO株式会社様の会社案内資料を制作させていただきました。</p>
+			<p class="design-txt-info">使用アプリケーション：Adobe Illustrator</p>
+		</div>
+	</div><!--/.design-wrap-->
+	
+	<div class="design-wrap">
+		<div class="design-pic"><img src="/img/design/dtp/fumi-co_booklet.png" alt="サービス紹介資料 / CROCO株式会社"></div>
+		<div class="design-txt">
+			<h3 class="design-txt-heading">サービス紹介資料 / CROCO株式会社</h3>
+			<p class="design-txt-description">渋谷区のコンテンツマーケティングソリューションを開発・提供しているCROCO株式会社様のサービス紹介資料を制作させていただきました。</p>
+			<p class="design-txt-info">使用アプリケーション：Adobe Illustrator</p>
+		</div>
+	</div><!--/.design-wrap-->
+	
+	<div class="design-wrap">
+		<div class="design-pic"><img src="/img/design/dtp/croco_interview.png" alt="サービス紹介チラシ / CROCO株式会社"></div>
+		<div class="design-txt">
+			<h3 class="design-txt-heading">サービス紹介チラシ / CROCO株式会社</h3>
+			<p class="design-txt-description">渋谷区のコンテンツマーケティングソリューションを開発・提供しているCROCO株式会社様のサービス紹介チラシを制作させていただきました。</p>
+			<p class="design-txt-info">使用アプリケーション：Adobe Illustrator</p>
+		</div>
+	</div><!--/.design-wrap-->
+	
+	<div class="design-wrap">
+		<div class="design-pic"><img src="/img/design/dtp/Pamphlet-omote.png" alt="サービス紹介パンフレット / フリービットEPARKヘルスケア"></div>
 		<div class="design-pic"><img src="/img/design/dtp/Pamphlet-ura.png" alt=""></div>
 		<div class="design-txt">
 			<h3 class="design-txt-heading">サービス紹介パンフレット / フリービットEPARKヘルスケア</h3>
@@ -44,7 +71,7 @@
 	</div><!--/.design-wrap-->
 	
 	<div class="design-wrap">
-		<div class="design-pic"><img src="/img/design/dtp/barinavi.png" alt=""></div>
+		<div class="design-pic"><img src="/img/design/dtp/barinavi.png" alt="サービス紹介チラシ / フリービットEPARKヘルスケア"></div>
 		<div class="design-txt">
 			<h3 class="design-txt-heading">サービス紹介チラシ / フリービットEPARKヘルスケア</h3>
 			<p class="design-txt-description">渋谷区の病院・薬局向けサービスを開発・提供している株式会社フリービットEPARKヘルスケア様のサービス紹介チラシを制作させていただきました。</p>
@@ -53,7 +80,7 @@
 	</div><!--/.design-wrap-->
 	
 	<div class="design-wrap">
-		<div class="design-pic"><img src="/img/design/dtp/okusuritecho.png" alt=""></div>
+		<div class="design-pic"><img src="/img/design/dtp/okusuritecho.png" alt="サービス紹介チラシ / フリービットEPARKヘルスケア"></div>
 		<div class="design-txt">
 			<h3 class="design-txt-heading">サービス紹介チラシ / フリービットEPARKヘルスケア</h3>
 			<p class="design-txt-description">渋谷区の病院・薬局向けサービスを開発・提供している株式会社フリービットEPARKヘルスケア様のサービス紹介チラシを制作させていただきました。</p>
@@ -62,7 +89,7 @@
 	</div><!--/.design-wrap-->
 	
 	<div class="design-wrap">
-		<div class="design-pic"><img src="/img/design/dtp/online.png" alt=""></div>
+		<div class="design-pic"><img src="/img/design/dtp/online.png" alt="サービス紹介チラシ / フリービットEPARKヘルスケア"></div>
 		<div class="design-txt">
 			<h3 class="design-txt-heading">サービス紹介チラシ / フリービットEPARKヘルスケア</h3>
 			<p class="design-txt-description">渋谷区の病院・薬局向けサービスを開発・提供している株式会社フリービットEPARKヘルスケア様のサービス紹介チラシを制作させていただきました。</p>
