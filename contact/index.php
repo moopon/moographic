81773647f3014c3f942c9c2929af6d257bdee9f7
黒木　庸平 <>
2025-05-07 10:48:17 +0900

    [update]フォームの調子が悪いので一旦削除。

--- contact/index.php
+++ contact/index.php
@@ -32,8 +32,7 @@
 	<h2 class="content-title">Contact</h2>
 	
 	<div class="contact-container">
-		<p class="contact-txt">イラストレーションのご相談や制作の依頼についてなど、お気軽にどうぞ。<br />以下のフォームにご記入頂くか、もしくは　
-			
+		<p class="contact-txt">イラストレーションのご相談や制作の依頼についてなど、お気軽にどうぞ。
 	<!--メールアドレス　START-->
 	<script type="text/javascript">
 	<!--
@@ -56,47 +55,6 @@
 		
 	<p class="contact-txt en">To inquire about my illustration, please email me at info[at]moographic[dot]jp.<br>Alternatively, please fill out the inquiry form.</p>	
 	
-		<form method="post" action="check.php" class="contact_form">
-		<ul class="contact_list">
-			<li class="contact_list-item">
-				<dl class="contact_list-box">
-					<dt class="contact_list-heading"><label for="uname">お名前<span class="required-icon">必須</span></label></dt>
-					<dd class="contact_list-field">
-						<input type="text" name="uname" id="uname" class="required" placeholder="Your name">
-					</dd>
-				</dl>
-			</li>
-			<li class="contact_list-item">
-				<dl class="contact_list-box">
-					<dt class="contact_list-heading"><label for="email">メールアドレス<span class="required-icon">必須</span></label></dt>
-					<dd class="contact_list-field">
-						<input type="text" name="email" id="email" class="required" placeholder="Email">
-						<span class="error_email"></span>
-					</dd>
-				</dl>
-			</li>
-			
-			<li class="contact_list-item">
-				<dl class="contact_list-box">
-					<dt class="contact_list-heading"><label for="company">貴社名</label></dt>
-					<dd class="contact_list-field">
-						<input type="text" name="company" id="company" placeholder="Company name">
-					</dd>
-				</dl>
-			</li>
-			<li class="contact_list-item">
-				<dl class="contact_list-box">
-					<dt class="contact_list-heading"><label for="message">内容<span class="required-icon">必須</span></label></dt>
-					<dd class="contact_list-field">
-						<textarea name="message" rows="4" cols="40" class="required" id="message" placeholder="Message"></textarea>
-					</dd>
-				</dl>
-			</li>
-		</ul><!--/#contact_list-->
-		<div id="submit_button"><input id="submit" type="submit" name="sub1" value="入力内容を確認する" ></div>
-		
-		
-	</form>
 	</div><!--/.contact-container-->	
 	
 	
